ff58a099553a06cbd36f327260809a22d5845d4d
EndSaidN <124702161+EndSaidN@users.noreply.github.com>
2023-10-17 12:23:23 +0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -118,13 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = "/media/download/"
+MEDIA_ROOT = BASE_DIR / 'media/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static/'),
 ]
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
