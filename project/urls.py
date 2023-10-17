6d735422d17688c5403ae0e41ee25ed22fd4146c
EndSaidN <124702161+EndSaidN@users.noreply.github.com>
2023-10-17 11:55:57 +0500

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -7,8 +7,7 @@ from project import settings
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_ROOT, documant_url=settings.STATIC_URL)
